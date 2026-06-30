d70dceb7723672b4c1d9efb7261fad975d96e3c5
Patrick Morpey <patrick@morphp.nl>
2026-06-30 10:34:34 +0200

    Add integer type constraints to `BasicTask` properties

--- src/BasicTask.php
+++ src/BasicTask.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\TaskRunner;
 
-use Medas\Core\{Interfaces\Uuid, Types\Binary, Types\Text};
+use Medas\Core\{Interfaces\Uuid, Types\Binary, Types\Integer, Types\Text};
 use Medas\EntityManager\{Attributes\Entity, Attributes\Id, Traits\Timestamps};
 
 #[Entity(store: 'tasks')]
@@ -23,8 +23,14 @@ class BasicTask implements Task
 
     public TaskStatus $status = TaskStatus::Pending;
     public \DateTime|null $scheduledAt = null;
+
+    #[Integer(maxValue: Integer::UNSIGNED_4_BYTE_MAX)]
     public int|null $retryAfter = null;
+
+    #[Integer(maxValue: Integer::UNSIGNED_1_BYTE_MAX)]
     public int|null $maxAttempts = null;
+
+    #[Integer(maxValue: Integer::UNSIGNED_1_BYTE_MAX)]
     public int $attempts = 0;
 
     #[Text(length: 16)]
